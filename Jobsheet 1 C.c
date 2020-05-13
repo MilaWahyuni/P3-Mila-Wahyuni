ea879752473d9b1f7b196e98222014440fda1ac9
MilaWahyuni <64572360+MilaWahyuni@users.noreply.github.com>
2020-05-13 03:44:48 -0700

    Create Jobsheet 1 C.c
    Membuat fungsi untuk menampilkan penjumlahan tgl dan bulan lahir

---
+++ Jobsheet 1 C.c
@@ -0,0 +1,15 @@
+#include<stdio.h>  
+int tanggal_lahir, bulan_lahir;
+void hasil() 
+{  
+tanggal_lahir = 18;  
+bulan_lahir = 02;  
+int penjumlahan;  
+penjumlahan = tanggal_lahir + bulan_lahir; 
+printf ("Hasil + %d", penjumlahan); 
+} 
+int main () 
+{  
+hasil(); 
+} 
+ 
