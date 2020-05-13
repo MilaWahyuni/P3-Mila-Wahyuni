2851f17dacb3500e6244c5f6fd415dfb9151c691
MilaWahyuni <64572360+MilaWahyuni@users.noreply.github.com>
2020-05-13 03:46:52 -0700

    Create Jobsheet 2 C++.cpp
    Membuat sebuah fungsi untuk menampilkan bilangan genap dari nol sampai dua nim trakhir

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+using namespace std; 
+int genap() 
+{ 
+cout<<"Bilangan Genap : "<<endl; 
+for(int i=0; i<=91; i++) 
+{   
+if (i%2 == 0) 
+{   
+cout<<" "<<i;  
+} 
+} 
+} 
+int main() 
+{  
+genap(); 
+} 
